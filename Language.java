de47536c0181761d0cb8200a0b67f8d4a383207e
Tjkarthik2 <tjkarthik2@gmail.com>
2025-01-04 12:55:45 +0530

    Java

---
+++ Language.java
@@ -0,0 +1,15 @@
+package Abstraction1;
+
+public interface Language {
+	/*
+	 * Assignment on Abstraction Interface:Language 
+	 * Methods:Python() Methods:Selenium() 
+	 * Exceution class: Automation 
+	 * call all the methods of interface in automation
+	 */
+	
+	public void Python();
+	public int Selenium();
+		
+
+}
